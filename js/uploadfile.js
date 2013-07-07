654ca788a148466f04b4bd974342aa0caeb9cac2
Insect of web <webuffer@foxmail.com>
2013-07-07 21:09:57 +0800

    5isbook
    ”我爱搜书“ 后台管理系统全部页面

---
+++ js/uploadfile.js
@@ -0,0 +1,5 @@
+$=function(id){return document.getElementById(id);}
+function mc(){var _i=$("uploadfile");_i.click();}
+function mm(){$("puf").value=(document.fileUp.fileName.value);}
+window.onload=function(){
+}
