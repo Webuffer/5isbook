b8ab7a2f414d0bcb506b33fc89ee9476ef0c8bcb
Insect of web <webuffer@foxmail.com>
2013-07-07 20:50:16 +0800

    Revert "5isbook v1.0"
    This reverts commit 512d1ed36c634c7337aa1c3ab02fb2f0a744cba7.

--- js/uploadfile.js
+++
@@ -1,5 +0,0 @@
-$=function(id){return document.getElementById(id);}
-function mc(){var _i=$("uploadfile");_i.click();}
-function mm(){$("puf").value=(document.fileUp.fileName.value);}
-window.onload=function(){
-}
